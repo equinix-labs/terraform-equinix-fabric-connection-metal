70b23d2a8370962439678419f8b4d1ed6c832aa6
ocobleseqx <oscar.cobles@eu.equinix.com>
2023-10-05 19:20:49 +0200

    updated to use always fabric billed instead of deprecated shared ports feature
    Signed-off-by: ocobleseqx <oscar.cobles@eu.equinix.com>

--- main.tf
+++ main.tf
@@ -1,6 +1,8 @@
 locals {
   metal_connection_name = coalesce(var.metal_connection_name, lower(format("shared-conn-%s-%s", var.fabric_destination_metro_code, random_string.this.result)))
   fabric_primary_name   = var.fabric_connection_name != "" ? var.fabric_connection_name : upper(format("METAL-%s-%s", var.fabric_destination_metro_code, random_string.this.result))
+  fabric_secondary_name = var.fabric_secondary_connection_name != "" ? var.fabric_secondary_connection_name : format("%s-SEC", local.fabric_primary_name)
+  network_edge_secondary_device_id = var.network_edge_secondary_device_id != "" ? var.network_edge_secondary_device_id : var.network_edge_device_id
 }
 
 data "equinix_metal_project" "this" {
@@ -23,43 +25,11 @@ resource "equinix_metal_connection" "this" {
   tags               = var.metal_connection_tags
   speed              = format("%dMbps", var.fabric_speed)
   vlans              = var.metal_connection_vlans
-  service_token_type = var.service_token_automation_feature_preview ? "z_side" : null
+  service_token_type = "z_side"
 }
 
 module "equinix-fabric-connection" {
-  source  = "equinix-labs/fabric-connection/equinix"
-  version = "0.3.1"
-
-  # required variables
-  notification_users = var.fabric_notification_users
-
-  # optional variables
-  name                      = local.fabric_primary_name
-  network_edge_id           = var.network_edge_device_id
-  network_edge_interface_id = var.network_edge_device_interface_id
-  port_name                 = var.fabric_port_name
-  vlan_stag                 = var.fabric_vlan_stag
-  service_token_id          = var.fabric_service_token_id
-  speed                     = var.fabric_speed
-  speed_unit                = "MB"
-  purchase_order_number     = var.fabric_purchase_order_number
-
-  zside_service_token_id   = var.service_token_automation_feature_preview ? equinix_metal_connection.this.service_tokens.0.id : null
-  seller_profile_name      = var.service_token_automation_feature_preview ? "" : var.redundancy_type == "SINGLE" ? "Equinix Metal - Layer 2" : "Equinix Metal - Layer 2 - Redundant"
-  seller_authorization_key = var.service_token_automation_feature_preview ? "" : equinix_metal_connection.this.token
-  seller_metro_code        = var.service_token_automation_feature_preview ? "" : var.fabric_destination_metro_code
-
-  redundancy_type                     = var.service_token_automation_feature_preview ? "SINGLE" : var.redundancy_type
-  secondary_name                      = var.service_token_automation_feature_preview ? "" : var.fabric_secondary_connection_name
-  secondary_port_name                 = var.service_token_automation_feature_preview ? "" : var.fabric_secondary_port_name
-  secondary_vlan_stag                 = var.service_token_automation_feature_preview ? 0 : var.fabric_secondary_vlan_stag
-  secondary_service_token_id          = var.service_token_automation_feature_preview ? "" : var.fabric_secondary_service_token_id
-  network_edge_secondary_id           = var.service_token_automation_feature_preview ? "" : var.network_edge_secondary_device_id
-  network_edge_secondary_interface_id = var.service_token_automation_feature_preview ? 0 : var.network_edge_secondary_device_interface_id
-}
-
-module "equinix-fabric-connection-sec" {
-  count   = var.service_token_automation_feature_preview && var.redundancy_type == "REDUNDANT" ? 1 : 0
+  count   = var.redundancy_type == "REDUNDANT" ? 2 : 1
   source  = "equinix-labs/fabric-connection/equinix"
   version = "0.4.0"
 
@@ -67,14 +37,17 @@ module "equinix-fabric-connection-sec" {
   notification_users = var.fabric_notification_users
 
   # optional variables
-  name                  = var.fabric_secondary_connection_name != "" ? var.fabric_secondary_connection_name : format("%s-SEC", local.fabric_primary_name)
-  port_name             = var.fabric_secondary_port_name != "" ? var.fabric_secondary_port_name : var.fabric_port_name
-  vlan_stag             = var.fabric_secondary_vlan_stag
-  speed                 = var.fabric_speed
+  name = count.index == 0 ? local.fabric_primary_name : local.fabric_secondary_name
+
+  network_edge_id           = count.index == 0 ? var.network_edge_device_id : local.network_edge_secondary_device_id
+  network_edge_interface_id = count.index == 0 ? var.network_edge_device_interface_id : var.network_edge_secondary_device_interface_id
+
+  port_name             = count.index == 0 ? var.fabric_port_name : var.fabric_secondary_port_name != "" ? var.fabric_secondary_port_name : var.fabric_port_name
+  vlan_stag             = count.index == 0 ? var.fabric_vlan_stag : var.fabric_secondary_vlan_stag
+  service_token_id      = count.index == 0 ? var.fabric_service_token_id : var.fabric_secondary_service_token_id
+  speed                 = count.index == 0 ? var.fabric_speed : var.fabric_secondary_speed != 0 ? var.fabric_secondary_speed : var.fabric_speed
   speed_unit            = "MB"
   purchase_order_number = var.fabric_purchase_order_number
 
-  zside_service_token_id = equinix_metal_connection.this.service_tokens.1.id
-
-  redundancy_type = "SINGLE"
+  zside_service_token_id = equinix_metal_connection.this.service_tokens[count.index].id
 }
--- outputs.tf
+++ outputs.tf
@@ -1,60 +1,51 @@
 output "fabric_connection_primary_uuid" {
   description = "Unique identifier of the connection."
-  value       = module.equinix-fabric-connection.primary_connection.uuid
+  value       = module.equinix-fabric-connection.0.primary_connection.uuid
 }
 
 output "fabric_connection_primary_name" {
   description = "Name of the connection."
-  value       = module.equinix-fabric-connection.primary_connection.name
+  value       = module.equinix-fabric-connection.0.primary_connection.name
 }
 
 output "fabric_connection_primary_status" {
   description = "Connection provisioning status."
-  value       = module.equinix-fabric-connection.primary_connection.status
+  value       = module.equinix-fabric-connection.0.primary_connection.status
 }
 
 output "fabric_connection_primary_provider_status" {
   description = "Connection provisioning provider status."
-  value       = module.equinix-fabric-connection.primary_connection.provider_status
+  value       = module.equinix-fabric-connection.0.primary_connection.provider_status
 }
 
 output "fabric_connection_primary_speed" {
   description = "Connection speed."
-  value       = module.equinix-fabric-connection.primary_connection.speed
+  value       = module.equinix-fabric-connection.0.primary_connection.speed
 }
 
 output "fabric_connection_primary_speed_unit" {
   description = "Connection speed unit."
-  value       = module.equinix-fabric-connection.primary_connection.speed_unit
-}
-
-output "fabric_connection_primary_seller_metro" {
-  description = "Connection seller metro code."
-  value       = module.equinix-fabric-connection.primary_connection.seller_metro_code
+  value       = module.equinix-fabric-connection.0.primary_connection.speed_unit
 }
 
 output "fabric_connection_secondary_uuid" {
   description = "Unique identifier of the secondary connection."
-  value = module.equinix-fabric-connection.secondary_connection != null ? module.equinix-fabric-connection.secondary_connection.uuid : (
-  var.service_token_automation_feature_preview && var.redundancy_type == "REDUNDANT") ? module.equinix-fabric-connection-sec.primary_connection.uuid : null
+  value = var.redundancy_type == "REDUNDANT" ? module.equinix-fabric-connection.1.primary_connection.uuid : null
 }
 
 output "fabric_connection_secondary_name" {
   description = "Name of the secondary connection."
-  value = module.equinix-fabric-connection.secondary_connection != null ? module.equinix-fabric-connection.secondary_connection.name : (
-  var.service_token_automation_feature_preview && var.redundancy_type == "REDUNDANT") ? module.equinix-fabric-connection-sec.primary_connection.name : null
+  value = var.redundancy_type == "REDUNDANT" ? module.equinix-fabric-connection.1.primary_connection.name : null
 }
 
 output "fabric_connection_secondary_status" {
   description = "Secondary connection provisioning status."
-  value = module.equinix-fabric-connection.secondary_connection != null ? module.equinix-fabric-connection.secondary_connection.status : (
-  var.service_token_automation_feature_preview && var.redundancy_type == "REDUNDANT") ? module.equinix-fabric-connection-sec.primary_connection.status : null
+  value = var.redundancy_type == "REDUNDANT" ? module.equinix-fabric-connection.1.primary_connection.status : null
 }
 
 output "fabric_connection_secondary_provider_status" {
   description = "Secondary connection provisioning provider status."
-  value = module.equinix-fabric-connection.secondary_connection != null ? module.equinix-fabric-connection.secondary_connection.provider_status : (
-  var.service_token_automation_feature_preview && var.redundancy_type == "REDUNDANT") ? module.equinix-fabric-connection-sec.primary_connection.provider_status : null
+  value = var.redundancy_type == "REDUNDANT" ? module.equinix-fabric-connection.1.primary_connection.provider_status : null
 }
 
 output "equinix_metal_connection_uuid" {
